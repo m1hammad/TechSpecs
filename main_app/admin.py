e0eb9e7be1333da776a38d3f25a179ad1a7ce347
Muhammad Hammad Riaz <hammad27r@hotmail.com>
2022-03-29 09:53:52 -0400

    workday 2

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,5 @@
+import imp
 from django.contrib import admin
-
+from main_app.models import Tech
 # Register your models here.
+admin.site.register(Tech)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tech(models.Model):
+    name = models.CharField(max_length=500)
+    processor = models.CharField(max_length=250)
+    display = models.CharField(max_length=500)
+    ram = models.CharField(max_length=100)
+    storage = models.CharField(max_length=100)
+    os = models.CharField(max_length=100)
+    price = models.FloatField()
+    image = models.CharField(default=None, blank=True, null=True, max_length=2000)
+        
