23c8982d35a5563e601e116a7348c7faeca2475e
Muhammad Hammad Riaz <hammad27r@hotmail.com>
2022-04-06 12:07:39 -0400

    heroku deployment

--- techspecs/settings.py
+++ techspecs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from threading import local
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,9 @@ WSGI_APPLICATION = 'techspecs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TechSpecs',
+        'NAME': '',
+        'USER':'',
+        'PASSWORD':'',
     }
 }
 
@@ -124,3 +127,6 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
